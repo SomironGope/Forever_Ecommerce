aee25df420b93db59609cbd11b08a476b63f6cb5
Somiron Gope <somirongope466@gmail.com>
2026-08-15 15:06:48 +0300

    iadded server port 5173 again

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx,png}'],
+  server: {
+   port: 5173,
+  },
+  
   plugins: [
     tailwindcss(),
   ],
